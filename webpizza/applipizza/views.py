750d586e32276ee49e547c132d34c77007b94dc1
Elena Ferreira <elena.ferreira@universite-paris-saclay.fr>
2023-09-19 20:47:42 +0200

    ajt dossier + pizzas.html

--- webpizza/applipizza/views.py
+++ webpizza/applipizza/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+#import des modèles
+from applipizza.models import Pizza
+
 # Create your views here.
+def pizzas(request) :
+
+    #récup des pizzas de la BD avec les memes instructions que dans le shell
+    lesPizzas = Pizza.objects.all()
+
+    #on retourne l'emplacement du template et, meme s'il ne sert pas cette fois,
+    #le parametre request ainsi que le contenu calculé (lesPizzas)
+    # sous forme de dict python
+    return render(
+        request,
+        'applipizza/pizzas.html',
+        {'pizzas' : lesPizzas}
+    )
