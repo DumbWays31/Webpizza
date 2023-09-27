28df04344dc70e4c9a660f625d7e036b0ecea4f6
Elena Ferreira <elena.ferreira@universite-paris-saclay.fr>
2023-09-27 15:12:58 +0200

    ajt une pizza

--- webpizza/applipizza/forms.py
+++ webpizza/applipizza/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from django.forms import ModelForm #formulaires automatiques
-from applipizza.models import Ingredient
+from applipizza.models import Ingredient, Pizza
 
 #form pour un nvl ingredient
 class IngredientForm(ModelForm) :
     class Meta : 
         model = Ingredient
         fields = ['nomIngredient']
+
+
+class PizzaForm(ModelForm) :
+    class Meta : 
+        model = Pizza
+        fields = ['nomPizza']
--- webpizza/applipizza/views.py
+++ webpizza/applipizza/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 
 #import des modèles
-from applipizza.models import Pizza
-from applipizza.models import Ingredient
-
-from applipizza.models import Composition
+from applipizza.models import Pizza, Ingredient, Composition
+from applipizza.forms import IngredientForm, PizzaForm
 
 
 # Create your views here.
@@ -66,3 +64,55 @@ def formulaireCreationIngredient(request) :
         request,
         'applipizza/formulaireCreationIngredient.html',
     )
+
+
+def creerIngredient(request) :
+    
+    #recup du form posté
+    form = IngredientForm(request.POST)
+
+    if form.is_valid() :
+        #recup de la valeur du champ "nomIngredient"
+        #form.cleaned_data nettoie la donnée au cas où des injections en tout genre seraient présentes
+        nomIng = form.cleaned_data['nomIngredient']
+
+        #creation du nvl ing
+        ing = Ingredient()
+
+        #affection de son attribut nomIngredient
+        ing.nomIngredient = nomIng
+
+        #enregistrement de l'ing dans la base
+        ing.save()
+
+        #on retourne l'emplacement de la vue (template au sens de django)
+        #et le contenu calculé sous forme d'un dict python
+        return render(
+            request,
+            'applipizza/traitementFormulaireCreationIngredient.html',
+            {"nom" : nomIng}
+        )
+
+
+def formulaireCreationPizza(request) :
+    #on retourn l'emplacement du template
+    return render(
+        request,
+        'applipizza/formulaireCreationPizza.html',
+    )
+
+def creerPizza(request) :
+    
+    form = PizzaForm(request.POST)
+
+    if form.is_valid() :
+        nomPiz = form.cleaned_data['nomPizza']
+        piz = Pizza()
+        piz.nomPizza = nomPiz
+        piz.save()
+        
+        return render(
+            request,
+            'applipizza/traitementFormulaireCreationPizza.html',
+            {"nom" : nomPiz}
+        )
