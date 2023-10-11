d7d59b9b7c098424e5337632d34ea4103f7c9217
Elena Ferreira <elena.ferreira@universite-paris-saclay.fr>
2023-10-11 15:38:04 +0200

    login

---
+++ webpizza/applicompte/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+#import des views par defaut du systm d'authentification de django, qui sera rename auth_views
+from django.contrib.auth import views as auth_views
+from applicompte import views
+
+urlpatterns: [
+    path('login/', auth_views.LoginView.as_view(template_name='applicompte/login.html'), name='login'),
+]
