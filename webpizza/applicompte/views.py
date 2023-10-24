01c4b38d530621e00174289209228ada97805716
Elena Ferreira <elena.ferreira@universite-paris-saclay.fr>
2023-10-24 17:25:18 +0200

    reinitialisation md

--- webpizza/applicompte/views.py
+++ webpizza/applicompte/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout 
 from applipizza.models import Pizza
 from applicompte.models import PizzaUser
@@ -7,15 +7,18 @@ from applicompte.forms import PizzaUserForm
 
 
 # Create your views here.
+
+#----------------------------#
+#  CONNEXION ET DECONNEXION  #
+#----------------------------#
 def connexion (request) :
     usr = request.POST['username']
     pwd = request.POST['password']
 
     user = authenticate(request, username = usr, password = pwd)
-    
-    user = PizzaUser.objects.get(id = user.id)
 
     if user is not None:
+        user = PizzaUser.objects.get(id = user.id)
         login(request, user)
         lesPizzas = Pizza.objects.all ()
         return render (
@@ -26,9 +29,9 @@ def connexion (request) :
     else:
         return render( 
             request,
-            'applicompte/login.html'
+            # 'applicompte/login.html'
+            'applicompte/erreur.html'
         )
-
 
 def deconnexion (request) :
     logout(request)
@@ -38,6 +41,10 @@ def deconnexion (request) :
     )
 
 
+
+#----------------------------#
+#           PROFIL           #
+#----------------------------#
 def formulaireProfil(request) :
     user = None
 
@@ -55,7 +62,6 @@ def formulaireProfil(request) :
             'applicompte/login.html'
         )
     
-
 def traitementFormulaireProfil(request, user_id) :
     user = None
 
@@ -79,3 +85,57 @@ def traitementFormulaireProfil(request, user_id) :
             request,
             'applicompte/login.html',
         )
+    
+
+
+#----------------------------#
+#       INSCRIPTION          #
+#----------------------------#
+def formulaireInscription(request) :
+    user = None
+
+    if request.user.is_authenticated : 
+        user = PizzaUser.objects.get(id = request.user.id)
+        return redirect("/pizzas/")
+
+    else :
+        return render(
+            request,
+            'applicompte/formulaireInscription.html'
+        )
+    
+def traitementFormulaireInscription(request) :
+    # récupération des champs du formulaire
+    fst = request.POST['first_name']
+    lst = request.POST['last_name']
+    usr = request.POST['username']
+    eml = request.POST['email']
+    pwd = request.POST['password']
+    img = request.FILES['image']
+
+    # création d'un PizzaUser
+    user = PizzaUser()
+
+    # affectation des champs récupérés aux attributs du PizzaUser
+    user.first_name = fst
+    user.last_name = lst
+    user.username = usr
+    user.email = eml
+    user.set_password(pwd)
+    user.image = img
+
+    # sauvegarde du PizzaUser dans la base de données 
+    user.save()
+
+    # connexion du PizzaUser
+    login(request, user)
+
+    # récupération des pizzas
+    lesPizzas = Pizza.objects.all()
+
+    # on renvoie l'appel du template, avec les pizzas et le PizzaUser
+    return render ( 
+        request,
+        'applipizza/pizzas.html', 
+        {"pizzas" : lesPizzas, "user" : user}
+    )
